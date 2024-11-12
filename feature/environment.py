b67db5e4d15fdc3e402412614739ecd78c0f8bfa
kevin hua <kevinghua2@gmail.com>
2024-11-11 23:41:13 -0600

    task 3

---
+++ feature/environment.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.wait import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.chrome.options import Options
+
+from app.application import Application
+
+
+def browser_init(context):
+    driver_path = ChromeDriverManager().install()
+    service = Service(driver_path)
+    context.driver = webdriver.Chrome(service=service)
+
+    context.driver.maximize_window()
+    context.driver.implicitly_wait(4)
+    context.driver.wait = WebDriverWait(context.driver, timeout=15)
+    context.app = Application(context.driver)
+
+
+def before_scenario(context, scenario):
+    print('\nStarted scenario: ', scenario.name)
